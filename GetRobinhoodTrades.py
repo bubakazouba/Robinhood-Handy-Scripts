5c943301e5eb15c9fe85297bebbd8e3e0c8d0fbb
Sahmoud <sahmoua@amazon.com>
2017-11-03 18:55:22 -0700

    adds caching to GetRobinhoodTrades.py

--- GetRobinhoodTrades.py
+++ GetRobinhoodTrades.py
@@ -1,37 +1,42 @@
 from Robinhood import Robinhood
-import getpass
-import collections
-import ast
+import pickle
+import os.path
 
 def getRobinhoodTrades(username, password, debug=False):
     robinhood = Robinhood()
 
     logged_in = robinhood.login(username=username, password=password)
     if logged_in == False:
-        print ("Invalid username or password.  Try again.\n")
+        print("Invalid username or password.  Try again.\n")
         return None
 
     trades = []
-    trade_count = 0
-    queued_count = 0
+    cached_instruments = {} #{instrument:symbol}
+    robinhood_cache = {}
+    last_page_url = None
 
-    # fetch order history and related metadata from the Robinhood API
-    orders = robinhood.get_endpoint('orders')
+    if os.path.isfile("robinhood_cache"):
+        robinhood_cache = pickle.load(open("robinhood_cache", "rb"))
+        last_page_url = robinhood_cache['last_page_url']
+        cached_instruments = robinhood_cache['instruments']
+        trades = robinhood_cache['trades']
+    
+    if not os.path.isfile("robinhood_cache") or last_page_url is None:
+        # fetch order history and related metadata from the Robinhood API
+        orders = robinhood.get_endpoint('orders')
+    else:
+        orders = robinhood.get_custom_endpoint(last_page_url)
 
     # do/while for pagination
 
     #pagination
     paginated = True
     page = 0
-
-    #cache instruments
-    cached_instruments = {} #{instrument:symbol}
-
+    length_of_trades_at_last_page = len(trades)
     while paginated:
         for i, order in enumerate(orders['results']):
             executions = order['executions']
             if len(executions) > 0:
-                trade_count += 1
                 # Iterate over all the different executions
                 for execution in executions:
                     # Get the Symbol of the order
@@ -50,36 +55,37 @@ def getRobinhoodTrades(username, password, debug=False):
                     trades[-1]['timestamp'] = execution['timestamp']
                     trades[-1]['quantity'] = execution['quantity']
                     trades[-1]['price'] = execution['price']
-            # If the state is queued, we keep this to let the user know they are pending orders
             elif order['state'] == "queued":
-                queued_count += 1
+                pass
 
-        # paginate, if out of ORDERS paginate is OVER
         if orders['next'] is not None:
             page = page + 1
             #get the next order, a page is essentially one order
             if debug:
                print (str(page) + "," + orders['next'])
-            orders = robinhood.get_custom_endpoint(str(orders['next']))
+            last_page_url = orders['next']
+            length_of_trades_at_last_page = len(trades)
+            orders = robinhood.get_custom_endpoint(orders['next'])
+            
         else:
             paginated = False
 
-
-
-    #trades stores ALL relevant information
+    robinhood_cache['last_page_url'] = last_page_url
+    robinhood_cache['instruments'] = cached_instruments
+    robinhood_cache['trades'] = trades[:length_of_trades_at_last_page] # dont include trades of the last page, because they will be queried for again
+    pickle.dump(robinhood_cache, open("robinhood_cache", "wb"))
 
     # check we have trade data to export
-    if trade_count > 0 or queued_count > 0:
-        print("%d queued trade%s and %d executed trade%s found in your account." % (queued_count, "s"[queued_count==1:], trade_count, "s"[trade_count==1:]))
-        # print str(queued_count) + " queded trade(s) and " + str(trade_count) + " executed trade(s) found in your account."
+    if len(trades) > 0:
+        print("%d executed trades found in your account." % len(trades))
     else:
         print("No trade history found in your account.")
         quit()
 
     # parse float keys    
-    floatKeys = ["price","fees","quantity"]
+    float_keys = ["price","fees","quantity"]
     for trade in trades:
-        for key in floatKeys:
+        for key in float_keys:
             try:
                 trade[key] = round(float(trade[key]),2)
             except:
